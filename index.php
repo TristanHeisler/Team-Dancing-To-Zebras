941d326c8720ba3aacce6535ca91ca1ac7c17da1
Tristan Heisler <TristanHeisler@hotmail.ca>
2018-02-13 13:53:03 -0500

    Made permission modifications so that all group members can perform web development easily.

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 	<title>Home</title>
 </header>
 <body>
-	This is the index page for our ENSE 470 Group Project
+	This is the index page for the ENSE 470 Group Team Dancing to Zebras. Future work will appear here.
 </body>
 </html>
