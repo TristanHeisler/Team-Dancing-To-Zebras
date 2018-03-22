9c71712c48a1feba9ca4dd95180a1622e5bf2b23
Tristan Heisler <tristanheisler@hotmail.ca>
2018-03-21 18:52:58 -0600

    Added functionality to email the requester once the analyst has made the final decision on their ticket.

--- processing/setAccess.php
+++ processing/setAccess.php
@@ -1,4 +1,7 @@
 <?php
+	//Required to send emails
+	require_once '../swift/lib/swift_required.php';
+
 	//Include the current session
   session_start();
 
@@ -24,33 +27,85 @@
 
   //Ensure that the approver has permission to make a decision concerning the given tool
   $sql = "SELECT\n"
-			. "	SoftwareRequest.requestID\n"
+			. "	SoftwareRequest.requestID,\n"
+			.	" SoftwareRequest.requesterName,\n"
+			. "	SoftwareRequest.requesterEmail,\n"
+			. "	SoftwareTool.name AS toolName\n"
 			. "FROM SoftwareRequest\n"
+			. "INNER JOIN SoftwareTool ON SoftwareRequest.softwareToolID = SoftwareTool.toolID\n"
       . "WHERE SoftwareRequest.requestID = " . $_GET['id'] . "\n";
     
-  $pendingTicket = mysqli_query($connection, $sql);
+  $ticketResults = mysqli_query($connection, $sql);
 
   //If no request with the given ID exists or the user is not an approver for the tool, return them to the view requests page
-  if(!mysqli_num_rows($pendingTicket))
+  if(!mysqli_num_rows($ticketResults))
   {
       header("location: ../viewTickets.php");
       exit;
   }
 
+	$pendingTicket = mysqli_fetch_assoc($ticketResults);
+	$requestID = $pendingTicket["requestID"];
+	$userName = $pendingTicket["requesterName"];
+	$userEmail = $pendingTicket["requesterEmail"];
+	$toolName = $pendingTicket["toolName"];
+
   //Free the result set
-	mysqli_free_result($pendingTicket);
+	mysqli_free_result($ticketResults);
 
   //Update the access status accordingly
   $sql = "UPDATE SoftwareRequest\n"
     . "SET accessStatus = '" . $_GET['a'] . "'\n"
     . "WHERE requestID = " . $_GET['id'] . "\n";
 
-  mysqli_query($connection, $sql);
+	//If the insertion was successful, continue the process of informing the user
+  if(mysqli_query($connection, $sql))
+	{	
+		//Close database connection
+		mysqli_close($connection); 	
+		
+		//Access an SMTP server
+		$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 587, "tls")
+			->setUsername('sseuofr@gmail.com')
+			->setPassword('Z9KqzY+J');
 
-  //Close the database connection
-  mysqli_close($connection);
+		//Create a mailer
+		$mailer = Swift_Mailer::newInstance($transport);
+		
+		//Create the email subject
+		$subject = 'HELL Software Tool Request #' . $requestID . ' Decision';
+		
+		//Create the beginning of email contents
+		$contents = "Hello " . $userName . ",\n\n"
+			. "Thank you for your interest in the " . $toolName . " tool.\n\n";
+		
+		//Populate the email based on if access was granted or not
+		if($_GET['a'] == "Approved")
+		{
+			$contents .= "We are happy to inform you that access to this tool has been granted.\n\n";
+		}
+		else
+		{
+			$contents .= "We regret to inform you that access to this tool has been denied.\n\n";
+		}
+		
+		//Finish the email contents
+		$contents .= "Thank you,\nHealth and Environment Labs Limited";
+		
+		//Create the message
+		$message = Swift_Message::newInstance($subject)
+			->setFrom(array('DoNotReply@HELL.org' => 'DoNotReply'))
+			->setTo(array($userEmail))
+			->setBody($contents);
 
-  //Return to the list of pending requests and inform the user
-  header("location: ../viewTickets.php?id=" . $_GET['id'] . "&a=" . $_GET['a']);
-	exit;
-?>
+		//Send the email
+		$result = $mailer->send($message);
+		
+		//Return to the list of pending requests and inform the analyst
+		header("location: ../viewTickets.php?id=" . $_GET['id'] . "&a=" . $_GET['a']);
+		exit;
+	}
+	else
+	{
+		echo "Error: " . mysqli_error($connection);
+	}
